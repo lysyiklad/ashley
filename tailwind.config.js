78d0d1a707204aba0b702db42e398872f350e3c1
lysyiklad <vadim.klad@gmail.com>
2024-03-13 18:21:39 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1440px',
+    // xl: { max: '1440px' },
+    // lg: { max: '976px' },
+    // md: { max: '768px' },
+    // sm: { max: '480px' },
+    // },
+    extend: {
+      screens: {
+        '2xl': '1440px',
+      },
+    },
   },
   plugins: [],
 };
